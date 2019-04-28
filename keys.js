9b8f9ceb84afd859302260360cd802bc2bf5abcd
CKayV11 <11303975+ckayv11@users.noreply.github.com>
2019-04-28 15:14:23 -0500

    updated findMovie and findConcert functions, added function doWhatItSays where LIRI takes text inside random.txt and prints result to console

--- keys.js
+++ keys.js
@@ -5,62 +5,95 @@ exports.spotify = {
   secret: process.env.SPOTIFY_SECRET
 };
 
-var axios = require("axios");
-var fs = require("fs");
+var axios = require('axios');
+var fs = require('fs');
 var moment = require('moment');
+// var spotify = new Spotify(keys.spotify);
+// var Spotify = require('node-spotify-api');
+
 
 var askLiri = function () {
-    // divider will be used as a spacer between the data we print in log.txt
-    var divider = "\n------------------------------------------------------------\n\n";
-
-    // findMovie takes in the name of a movie and searches the OMDB API
-    this.findMovie = function (movie) {
-        var URL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
-    
-        axios.get(URL).then(function (response) {
-          // Place the response.data into a variable, jsonData.
-          var jsonData = response.data;
-          // movieData ends up being the string containing the movie data we will print to the console
-          var movieData = [
-            "Movie Title: " + jsonData.Title,
-            "Release Year: " + jsonData.Year,
-            "IMDB Rating: " + jsonData.Ratings[0].Value,
-            "Rotten Tomatoes Rating: " + jsonData.Ratings[1].Value,
-            "Country: " + jsonData.Country,
-            "Language: " + jsonData.Language,
-            "Plot: " + jsonData.Plot,
-            "Actors: " + jsonData.Actors,
-          ].join("\n\n");
-    
-          // Append concertData and the divider to log.txt, print concertData to the console
-          fs.appendFile("log.txt", movieData + divider, function (err) {
-            if (err) throw err;
-            console.log(movieData);
-          });
-        });
-      };
-
-    //findConcert takes in the name of a band and searches the Bands in Town Artist Events API
-    this.findConcert = function (artist) {
-      var URL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-
-      axios.get(URL).then(function (response) {
-        // Place the response.data into a variable, jsonData.
-        var jsonData = response.data[0];
-        // concertData ends up being the string containing the concert data we will print to the console
-        var concertData = [
-          "Artist: " + artist,
-          "Venue Name: " + jsonData.venue.name,
-          "Venue Location: " + jsonData.venue.city + ", " + jsonData.venue.region,
-          "Date of Event: " + moment(jsonData.datetime).format('l'),
-        ].join("\n\n");
-
-      //   Append concertData and the divider to log.txt, print concertData to the console
-        fs.appendFile("log.txt", concertData + divider, function (err) {
-          if (err) throw err;
-          console.log(concertData);
-        });
-      });
+  // divider will be used as a spacer between the data we print in log.txt
+  var divider = "\n------------------------------------------------------------\n\n";
+  
+  // findMovie takes in the name of a movie and searches the OMDB API
+  this.findMovie = function (movie) {
+    if (!movie) {
+      console.log("Need a movie recommendation? Check this one out on Netflix!")
+      movie = "Mr Nobody";
     };
-}
-module.exports = askLiri;
+    var URL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+    axios.get(URL).then(function (response) {
+      // Place the response.data into a variable, jsonData.
+      var jsonData = response.data;
+      // movieData ends up being the string containing the movie data we will print to the console
+      var movieData = [
+        "Movie Title: " + jsonData.Title,
+        "Release Year: " + jsonData.Year,
+        "IMDB Rating: " + jsonData.Ratings[0].Value,
+        "Rotten Tomatoes Rating: " + jsonData.Ratings[1].Value,
+        "Country: " + jsonData.Country,
+        "Language: " + jsonData.Language,
+        "Plot: " + jsonData.Plot,
+        "Actors: " + jsonData.Actors,
+      ].join("\n\n");
+      // Append movieData and the divider to log.txt, print movieData to the console
+      fs.appendFile("log.txt", movieData + divider, function (err) {
+        if (err) throw err;
+        console.log(movieData);
+      });
+    });
+  };
+
+  //findConcert takes in the name of a band and searches the Bands in Town Artist Events API
+  this.findConcert = function (artist) {
+    if (!artist) {
+      console.log("Can't think of any concerts? Check out this boy band - they're dope!")
+      artist = "Backstreet Boys";
+    };
+    var URL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    axios.get(URL).then(function (response) {
+      var jsonData = response.data[0];
+      var concertData = [
+        "Artist: " + artist,
+        "Venue Name: " + jsonData.venue.name,
+        "Venue Location: " + jsonData.venue.city + ", " + jsonData.venue.region,
+        "Date of Event: " + moment(jsonData.datetime).format('l'),
+      ].join("\n\n");
+      fs.appendFile("log.txt", concertData + divider, function (err) {
+        if (err) throw err;
+        console.log(concertData);
+      });
+    });
+  };
+
+  //findSong takes in the name of a song and searches Spotify API
+  // this.findSong = function (song) {
+  //   // If there is no song name, set the song to Ace of Base "The Sign"
+  //   if (!song) {
+  //     song = "The Sign";
+  //   }
+  //   spotify.search({ type: 'track', query: song }, function (err, data) {
+  //     if (err) {
+  //       console.log('Error occurred: ' + err);
+  //       return;
+  //     } else {
+  //       var trackData = data.tracks.items[0];
+  //       var songData = [
+  //         "Artist(s): " + trackData.album.artists[0].name,
+  //         "Song Name: " + song,
+  //         "Song Preview: " + trackData.album.external_urls.spotify,
+  //         "Album: " + trackData.album.name,
+  //       ].join("\n\n");
+
+  //       //   Append songData and the divider to log.txt, print songData to the console
+  //       fs.appendFile("log.txt", songData + divider, function (err) {
+  //         if (err) throw err;
+  //         console.log(songData);
+  //       });
+  //     };
+  //   });
+  // };
+
+};
+  module.exports = askLiri;
--- liri.js
+++ liri.js
@@ -1,25 +1,48 @@
 require("dotenv").config();
-var askLiri = require("./keys.js");
+var keys = require("./keys.js");
+// var spotify = new Spotify(keys.spotify);
+var fs = require('fs');
 
-var event = new askLiri();
+var event = new keys();
 
 // Grab search command line argument
 var search = process.argv[2];
 // Joining the remaining arguments since request may contain spaces
 var term = process.argv.slice(3).join(" ");
 
-// Print searching for a movie & print the term as well
+// Print movie info
 if (search === "movie-this"){
     console.log("Searching for Movie");
     event.findMovie(term);
-} if (!term) {
-    console.log("Movie not found, but here's something better!");
-    term = "Mr. Nobody";
-    event.findMovie(term);
-}
+};
 
-// Print searching for a concert & print the term as well
+// Print concert info
 if (search === "concert-this") {
     console.log("Searching for Concert");
     event.findConcert(term);
-}
+};
+
+// Print song info
+if (search === "spotify-this-song"){
+    console.log("Searching for Song");
+    event.findSong(term);
+};
+
+
+//doWhatItSays function where LIRI takes the text inside of random.txt and then use it to call one of LIRI's commands.
+function doWhatItSays () {
+    //Reads the random.txt file and calls Spotify function
+    fs.readFile("random.txt", "utf8", function(err, data) {
+        // If the code experiences any errors it will log the error to the console.
+        if (err) {
+        return console.log(err);
+        };
+        event.findMovie(data);
+    });
+};
+// Print whatever is in do-what-it-says
+if (search === "do-what-it-says"){
+console.log("Searching...");
+doWhatItSays();
+};
+
